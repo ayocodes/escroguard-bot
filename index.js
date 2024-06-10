781d4dcc283afcfbf4c9c27b784f6ddac66eea77
Ayo <omotolatomi01@gmail.com>
2024-06-10 13:07:18 +0100

    work on telegram bot

--- index.js
+++ index.js
@@ -9,25 +9,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const grammy_1 = require("grammy");
-const menu_1 = require("@grammyjs/menu");
-// Create a bot.
-const bot = new grammy_1.Bot("6718845274:AAHmLDgK2Ef3zYWH3DAqlvSN9QLG7pbDYk8");
-// Create a simple menu.
-const menu = new menu_1.Menu("root-menu")
-    .text("Welcome", (ctx) => ctx.reply("Hi!"))
-    .row()
-    .submenu("Credits", "credits-menu");
-const settings = new menu_1.Menu("credits-menu")
-    .text("Show Credits", (ctx) => ctx.reply("Powered by grammY"))
-    .back("Go Back");
-menu.register(settings);
-// Make it interactive.
-bot.use(menu);
-bot.command("start", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    // Send the menu.
-    yield ctx.reply("Escroguard bot features \n click on one of the buttons!", {
-        reply_markup: menu,
-    });
+const bot_1 = require("./bot");
+const rootMenu_1 = require("./menus/rootMenu");
+const startCommand_1 = require("./commands/startCommand");
+const welcomeCommand_1 = require("./commands/welcomeCommand");
+const buyCommand_1 = require("./commands/buyCommand");
+const sellCommand_1 = require("./commands/sellCommand");
+bot_1.bot.use(rootMenu_1.rootMenu);
+bot_1.bot.command("start", startCommand_1.handleStartCommand);
+bot_1.bot.command("sell", sellCommand_1.handleSellCommand);
+bot_1.bot.on("message:text", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    if (ctx.session.currentStep === "awaitingName") {
+        yield (0, welcomeCommand_1.handleWelcomeMessage)(ctx);
+    }
+    else if (ctx.session.currentStep === "awaitingToken") {
+        yield (0, buyCommand_1.handleBuyToken)(ctx);
+    }
 }));
-bot.start();
+bot_1.bot.start();
+// import { Bot } from "grammy";
+// import { Menu } from "@grammyjs/menu";
+// // Create a bot.
+// const bot = new Bot("6718845274:AAHmLDgK2Ef3zYWH3DAqlvSN9QLG7pbDYk8");
+// // Create a simple menu.
+// const menu = new Menu("time")
+//   .text(
+//     "What's the time?",
+//     (ctx) => ctx.editMessageText("It is " + new Date().toLocaleString()),
+//   );
+// // Make it interactive.
+// bot.use(menu);
+// bot.command("start", async (ctx) => {
+//   // Send the menu.
+//   await ctx.reply("Check out this menu:", { reply_markup: menu });
+// });
+// bot.start();
